ef4b2395fcfc5ae4b45841996aa5c88754762df2
Vikki <vbro1293@gmail.com>
2018-03-19 09:29:17 +0000

    Player component added

--- src/components/PingPong.jsx
+++ src/components/PingPong.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Input from "./Input";
-import Score from "./Score";
+import Player from './Player';
 
 //Map over the players creating a input and score component for each
 //Passing in the Players as a prop and deconstructing to get each players name and score
@@ -8,10 +7,7 @@ import Score from "./Score";
 const PingPong = ({ players }) => (
 	<div className="row">
 		{ players.map((player, i) => (
-			<div key={ i } className="col-sm-6">
-				<Input name ={ player.get("name") } />
-				<Score score={ player.get("score") } />
-			</div>
+			<Player key={ i } player={ player } />
 		))}
 	</div>
 )
