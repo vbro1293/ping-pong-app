1e8f630490e727f46aa3fbabbfa91a3e6beb62b7
Vikki <vbro1293@gmail.com>
2018-03-20 09:30:32 +0000

    functions in reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,4 +1,23 @@
+import { UPDATE_NAME, INCREMENT_SCORE };
+
 //Variables for number of serves each and winning score
 
 const servesEach = 5;
 const endOn = 21;
+
+//Update specified players name
+const updateName = (state, { player, name }) => state.setIn(["players", player, "name"], name);
+
+//Add 1 to players score
+const incrementScore = (state, { player }) => state.updateIn(["players", player, "score"], score => score+1);
+
+//switch statement to call function depending on action type
+const state = (state, action) => {
+	switch (action.type){
+		case UPDATE_NAME: return updateName(state, action);
+		case INCREMENT_SCORE: return incrementScore(state, action);
+		default: return state;
+	}
+}
+
+export default state;
