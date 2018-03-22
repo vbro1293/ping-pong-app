86cbad968c13dd822dc1846f25949fdfabe960a7
Vikki <vbro1293@gmail.com>
2018-03-22 08:14:43 +0000

    added store in indexjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import store from "./data/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from "react-redux";
+
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById("root"),
+);
